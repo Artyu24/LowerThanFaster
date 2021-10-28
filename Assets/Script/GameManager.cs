9cfaa36575b3626a919e30d9a562cdd673ece752
Arthur <65313164+Artyu24@users.noreply.github.com>
2021-10-28 18:26:45 +0200

    Add systeme Object Spawn

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,12 +10,51 @@ public class GameManager : MonoBehaviour
     public bool isDead;
     public bool victory;
 
+    public int nbrObjectToFind;
+
+    [Header("Object")]
     public GameObject[] listObject;
+
+    [Header("Spawn Point")]
+    public GameObject[] spawnPointObject;
+
+    private GameObject[] objectToFind;
 
     private void Start()
     {
         if (instance == null)
             instance = this;
+
+        //Spawn des objets en random !
+        foreach (GameObject collectable in listObject)
+        {
+            int random = Random.Range(0, spawnPointObject.Length);
+            while (spawnPointObject[random].transform.position == new Vector3(0, 0, 0))
+                random = Random.Range(0, spawnPointObject.Length);
+            Instantiate(collectable, spawnPointObject[random].transform.position, Quaternion.identity);
+            spawnPointObject[random].transform.position = new Vector3(0,0,0);
+            Destroy(spawnPointObject[random]);
+        }
+
+        //Créé une liste contenant les objets à trouver !
+        objectToFind = new GameObject[nbrObjectToFind];
+        for(int i = 0; i < nbrObjectToFind; i++)
+        {
+            int randomItem = Random.Range(0, listObject.Length);
+            int test = 0;
+            while (test != objectToFind.Length)
+            {
+                test = 0;
+                foreach(GameObject item in objectToFind)
+                {
+                    if (item == listObject[randomItem])
+                        randomItem = Random.Range(0, listObject.Length);
+                    else
+                        test++;
+                }
+            }
+            objectToFind[i] = listObject[randomItem];
+        }
     }
 
     void Update()
