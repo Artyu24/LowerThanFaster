54ccf413b92be5a5cb7a9c068e17878f5821b01d
Arthur <65313164+Artyu24@users.noreply.github.com>
2021-10-29 19:28:42 +0200

    Debut de jeu

--- Assets/Script/Collectables.cs
+++ Assets/Script/Collectables.cs
@@ -6,99 +6,39 @@ using UnityEngine.UI;
 public class Collectables : MonoBehaviour
 {
 
-    private bool isInRange; 
+    private bool isInRange;  
     public Text message; //indication "appuyer sur F"
 
     private GameObject objet = null; //le dernier objet rencontré par le joueur
     private string nomObjet; //son nom 
-    private bool enDestruction=false; 
+    private bool enDestruction=false;
 
-    public Image visuel1; //images dans l'inventaire de chacun des objets à ramasser
-    public Image visuel2;
-    public Image visuel3;
-    public Image visuel4;
-    public Image visuel5;
-    public Image visuel6;
-    public Image visuel7;
-    public Image visuel8;
-    public Image visuel9;
-    public Image visuel10;
-
-    private int nbImages = 0; // chaque case de l'inventaire prise oblige les autres à se decaler
-    Vector3 delta = new Vector3(50, 0, 0); //decalement de x=+70 vers la droite pour chaque enplacement occupé
-    public Vector3 positionInventaire = new Vector3(-170, 90, 0); //position du 1er emplacement inventaire
-
-    void Start()
-    {
-        
-    }
+    public Image[] visuel;
+    private int placeUI=0;
+    private bool isInList=false;
 
     void Update()
     {
         if (isInRange && Input.GetKeyDown(KeyCode.F)) //le joueur recupere un objet
         {
-            
+            isInList = false;
+            foreach (GameObject truc in GameManager.instance.objectToFind)
+            {
+                if (truc.GetComponent<SpriteRenderer>().sprite == objet.GetComponent<SpriteRenderer>().sprite)
+                {
+                    visuel[placeUI].sprite = objet.GetComponent<SpriteRenderer>().sprite;
+                    visuel[placeUI].gameObject.SetActive(true);
+                    placeUI++;
+                    isInList  =  true;
+                }
+                
+            }
+            if (isInList==false)
+            {
+                //malus à rajouter
+            }
             StartCoroutine(Obtenu());
             Destroy(objet);// un message "est obtenu" reste 2 secondes
-            switch (nomObjet) //l'image de l'objet apparait dans l'inventaire. Def de sa position, l'active, et sort du switch.
-            {
-                case "Objet1":  { 
-                        visuel1.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel1.gameObject.SetActive(true); 
-                        break; }
-                case "Objet2": { 
-                        visuel2.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel2.gameObject.SetActive(true);
-                        break; }
-                case "Objet3": { 
-                        visuel3.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel3.gameObject.SetActive(true);
-                        break; }
-                case "Objet4": { 
-                        visuel4.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel4.gameObject.SetActive(true); 
-                        break; }
-                case "Objet5":
-                    {
-                        visuel5.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel5.gameObject.SetActive(true);
-                        break;
-                    }
-                case "Objet6":
-                    {
-                        visuel6.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel6.gameObject.SetActive(true);
-                        break;
-                    }
-                case "Objet7":
-                    {
-                        visuel7.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel7.gameObject.SetActive(true);
-                        break;
-                    }
-                case "Objet8":
-                    {
-                        visuel8.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel8.gameObject.SetActive(true);
-                        break;
-                    }
-                case "Objet9":
-                    {
-                        visuel9.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel9.gameObject.SetActive(true);
-                        break;
-                    }
-                case "Objet10":
-                    {
-                        visuel10.rectTransform.localPosition = positionInventaire + delta * nbImages;
-                        visuel10.gameObject.SetActive(true);
-                        break;
-                    }
-
-
-            }
-            nbImages++; //un objet recupéré en plus
-            
         }
     }
 
